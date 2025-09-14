2f61c9a7cd3f23bde4704db425495df3d357f013
Hache <thewhitetrackproject@proton.me>
2025-09-14 15:48:23 +0200

    Add PyQt application entry point

---
+++ songsearch/__main__.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from .app import MainWindow
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
